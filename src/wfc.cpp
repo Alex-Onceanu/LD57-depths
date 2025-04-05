a36ad043d66b01f5a716ac6c688afa828b53f976
Tancrède Côte <tancrede.cote@telecom-paris.fr>
2025-04-05 20:57:07 +0200

    debug

--- src/wfc.cpp
+++ src/wfc.cpp
@@ -15,59 +15,56 @@ Wfc::Wfc(int _tile_shapes, int _materials)
 
 void Wfc::update_one_step(std::vector<std::vector<Tile>> world, int x, int y, int *verified, int x_prec, int y_prec, int dir)
 {
-    for(int i = 0; i < n; i++)
-    {
-        if(i % width == 0) std::cout << std::endl;
-        std::cout << verified[i] << " ";
-    }
-    std::cout << std::endl;
-    if (!verified[y * width + x])
-    {
-        std::cout << "not verified !" << std::endl;
-
-        std::vector<Tile> newPossibilities;
-        std::vector<int> idPossibilities;
-
-        std::cout << "On veut acceder a l'element" << y_prec * width + x_prec << "d'un vector de taille " << world.size() << std::endl;
-
-        for (auto &tl : world[y_prec * width + x_prec])
+    if (!has_wfc_ended(verified, n)){
+        for(int i = 0; i < n; i++)
         {
-            std::cout << "On veut acceder a l'element " << y * width + x << " d'un vecteur de taille " << world.size()<<std::endl;
-            auto tv = tl.compatible_list(world[y * width + x], dir);
+            if(i % width == 0) std::cout << std::endl;
+            std::cout << verified[i] << " ";
+        }
+        std::cout << std::endl;
+        if (!verified[y * width + x] and 0 <=x and x<width and 0<=y and y<height)
+        {
+            std::cout << "not verified !" << std::endl;
 
-            for (auto &t2 : tv)
+            std::vector<Tile> newPossibilities;
+            std::vector<int> idPossibilities;
+
+            std::cout << "On veut acceder a l'element" << y_prec * width + x_prec << "d'un vector de taille " << world.size() << std::endl;
+
+            for (auto &tl : world[y_prec * width + x_prec])
             {
-                if (std::find(idPossibilities.begin(), idPossibilities.end(), t2.getId()) == idPossibilities.end())
+                std::cout << "On veut acceder a l'element " << y * width + x << " d'un vecteur de taille " << world.size()<<std::endl;
+                auto tv = tl.compatible_list(world[y * width + x], dir);
+
+                for (auto &t2 : tv)
                 {
-                    // std::cout << "On ajoute ";
-                    // printTile(t2.sides);
-                    // std::cout << "a new possibilities" << std::endl;
-                    newPossibilities.push_back(t2);
-                    idPossibilities.push_back(t2.getId());
+                    if (std::find(idPossibilities.begin(), idPossibilities.end(), t2.getId()) == idPossibilities.end())
+                    {
+                        // std::cout << "On ajoute ";
+                        // printTile(t2.sides);
+                        // std::cout << "a new possibilities" << std::endl;
+                        newPossibilities.push_back(t2);
+                        idPossibilities.push_back(t2.getId());
+                    }
                 }
             }
-        }
-        verified[y * width + x] = 1;
-        // std::cout << "Taille finale de new possibilites : " << newPossibilities.size() << std::endl;
+            verified[y * width + x] = 1;
+            // std::cout << "Taille finale de new possibilites : " << newPossibilities.size() << std::endl;
 
-        for (int i = 0; i < 4; i++)
-        {
-            if (i != (dir + 2) % 4)
+            for (int i = 0; i < 4; i++)
             {
-                update_one_step(world, x + (1 - i) * ((i + 1) % 2), y + (-i) * ((i) % 2), verified, x, y, i);
+                if (i != (dir + 2) % 4 and 0 <=x + (1 - i) * ((i + 1) % 2) and x + (1 - i) * ((i + 1) % 2)<width and 0<=y + (-i) * ((i) % 2) and y + (-i) * ((i) % 2)<height)
+                {
+                    update_one_step(world, x + (1 - i) * ((i + 1) % 2), y + (-i) * ((i) % 2), verified, x, y, i);
+                }
             }
         }
     }
 }
 
-void Wfc::update(std::vector<std::vector<Tile>> world, int x, int y)
+void Wfc::update(std::vector<std::vector<Tile>> world, int x, int y, int* verified)
 {
-    int *verified = new int[n];
-    for (int i = 0; i < n; i++)
-    {
-        verified[i] = 0;
-    }
-    verified[y * width + x] = 1;
+    
 
     std::cout << "J'ai toujours pas planté après le malloc douteux" << std::endl;
 
@@ -79,8 +76,15 @@ void Wfc::update(std::vector<std::vector<Tile>> world, int x, int y)
     {
         update_one_step(world, x + 1, y, verified, x, y, 0);
     }
+}
 
-    delete[] verified;
+bool Wfc::has_wfc_ended(int* verified, int n){
+    bool chosen = true;
+    for (int i = 0; i < n; i++)
+    {
+        chosen = chosen && (verified[i] == 1);
+    }
+    return chosen;
 }
 
 bool Wfc::has_wfc_ended(std::vector<std::vector<Tile>> world)
@@ -159,7 +163,12 @@ std::vector<Tile> Wfc::wfc(int _width, int _height, int _n)
         // copy(full.begin(),full.end(), back_inserter(world[i]));
         // printf("world est de longueur %d\n", world.size());
     }
-    while (!has_wfc_ended(world))
+    int *verified = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        verified[i] = 0;
+    }
+    while (!has_wfc_ended(verified, n))
     {
         int k = rand() % n;
 
@@ -176,9 +185,11 @@ std::vector<Tile> Wfc::wfc(int _width, int _height, int _n)
         world[jsp].push_back(temp);
 
         std::cout << "Go update !" << std::endl;
-        update(world, jsp % width, jsp / width);
+        verified[jsp] = 1;
+        update(world, jsp % width, jsp / width, verified);
     }
     std::cout << "Longueur de world : " << world.size() << std::endl;
+    delete[] verified;
 
     // for(int i = 0; i < materials * tile_shapes + 1; i++)
     // {
