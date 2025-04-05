1e0346c5a9f45edbcc10f38354fbda1ee130b397
Tancrède Côte <tancrede.cote@telecom-paris.fr>
2025-04-05 14:41:37 +0200

    le commit de ses morts

---
+++ src/tile.hpp
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
+#include <cstdio>
+
+
+class Tile{
+    private : 
+        int sides[4];
+        int id;
+        //string sprite; nom du .png
+    public : 
+        Tile(int id, int sides[4]); //string sprite
+        bool compatible(Tile other, int dir); //dir = 0 si other est à droite, 1 si other est en dessous, 2 si other est à gauche, ou 3 si other est au dessus de this 
+        std::vector<Tile> compatible_list(std::vector<Tile> t_list, int dir);
+};
