c4f086ba2085c57b35516c8323611c303ba302c7
Tancrède Côte <tancrede.cote@telecom-paris.fr>
2025-04-07 16:09:03 +0200

    configuration des murs limite

--- src/world.cpp
+++ src/world.cpp
@@ -20,9 +20,11 @@ World::World()
     {
         initial[j] = Tile(0, 0, 0, 0);
     }
-    for (int i = 1; i< NB_TILES_Y; i++){
-        initial[i*NB_TILES_X] = Tile(0,0,3,3);
-        initial[(i+1)*NB_TILES_X-1] = Tile(3,3,0,0);
+    initial [NB_TILES_X] = Tile(0,1,3,0);
+    initial [2*NB_TILES_X-1] = Tile(0,3,1,0);
+    for (int i = 2; i< NB_TILES_Y; i++){
+        initial[i*NB_TILES_X] = Tile(1,1,3,3);
+        initial[(i+1)*NB_TILES_X-1] = Tile(3,3,1,1);
     }
     // for(int i = 0; i < NB_TILES_Y; i++)
     // {
